d7e14b105817f13f00157e51af7b3041f687489a
A. Thompson <act.webdesigns@gmail.com>
2018-06-02 16:31:17 -0500

    changed the renderBackButton method on AppBar to a function.

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -5,22 +5,22 @@ import './AppBar.css';
 
 import {icon_back, icon_map} from '../../assets';
 
-export default class AppBar extends Component {
-  renderBackButton(bool) {
-    return (
+function conditionalBackBtn(bool) {
+   return (
       bool
       ? <Link to="/" className='BackButton'>
           <img src={icon_back} alt='Go back on mobile. Close tab on Desktop' />
         </Link> 
       : <div className="BackButton"/>
     )
-  }
+};
 
+export default class AppBar extends Component {
   render() {
     const {backButton} = this.props;
     return (
       <div className="AppBar">
-        {this.renderBackButton(backButton)}
+        {conditionalBackButton(backButton)}
         <Header variant="h2" inverted>
           Lunch Tyme
         </Header>
