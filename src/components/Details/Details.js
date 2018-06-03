2864d8908546a5284f531c86d07ae559fc390ee8
A. Thompson <act.webdesigns@gmail.com>
2018-06-03 17:07:59 -0500

    Added sass preprocessing;TODO:Swapping flexbox for grid in feed css.

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -5,7 +5,7 @@ import './Details.css';
 export default class Details extends Component {
   render() {
     const {data} = this.props;
-    return (data === undefined
+    return (data === undefined || data === null
       ? (<Redirect to="/"/>)
       : (
         <div className="Details">
@@ -13,11 +13,35 @@ export default class Details extends Component {
           <div className="DetailsHeaders">
             <h4>
               {data.name}
-              <span className="DetailsSubheader">{data.category}</span>
+              <span className="DetailsSubheader">
+              {data.category}
+              </span>
             </h4>
           </div>
+          <div className="DetailsInfo">
+            <ul>
+              <li>
+                {`
+                ${data.location.address} \n
+                ${data.location.city}, ${data.location.state}
+                ${data.location.postalCode} 
+              `}
+              </li>
+              {data.contact !== null && <React.Fragment>
+                <li>
+                  <a href={`tel:${data.contact.phone}`}>
+                    {data.contact.formattedPhone}
+                  </a>
+                </li>
+                <li>
+                  <a href={`https://twitter.com/${data.contact.twitter}`}>
+                    @{data.contact.twitter}
+                  </a>
+                </li>
+              </React.Fragment>}
+            </ul>
+          </div>
         </div>
-      )
-    )
+      ))
   }
 }
--- src/components/LunchTyme/LunchTyme.js
+++ src/components/LunchTyme/LunchTyme.js
@@ -2,29 +2,44 @@ import React, {Component} from 'react';
 import {Link, Route} from 'react-router-dom';
 
 import './LunchTyme.css';
-import {Loader, Image} from 'semantic-ui-react';
+import {Loader} from 'semantic-ui-react';
 import {Details, AppBar} from '../../components';
 
-const dataUrl = 'http://sandbox.bottlerocketapps.com/BR_iOS_CodingExam_2015_Server/restaurants.json';
+const dataUrl = 'http://sandbox.bottlerocketapps.com/BR_iOS_CodingExam_2015_Server/restaurants.json'; 
 
 const createFeed = (feed) => {
   let arr = [];
   const restaurants = feed.restaurants.map(restaurant => {
-    const {name, backgroundImageURL, category} = restaurant;
-    let formattedName = restaurant.name.replace(/ +/g, '-');
-    arr.push(restaurant);
-    return (
-      <Image 
-        as={Link} 
-        to={`/restaurant/${arr.length - 1}/${formattedName}`}
-        className="Restaurant"
-        key={arr.length - 1}
-        src={restaurant.backgroundImageURL}
-        alt={`Link to details for ${restaurant.name}`}
-        wrapped >
-      </Image>
-    );
-  });
+      const {name, backgroundImageURL, category} = restaurant;
+      const style = {
+        background: `url(${backgroundImageURL})` 
+      };
+      let formattedName = restaurant.name.replace(/ +/g, '-');
+
+      // arr is where the data is stored,
+      //   and then matched with the 
+      //   corresponding feed item.
+      arr.push(restaurant);
+
+      // this returns back an array of
+      //   componenants that are rendered
+      //   from a map of the data.
+      return (
+        <Link
+          to={`/restaurant/${arr.length - 1}/${formattedName}`}
+          className="Restaurant"
+          key={arr.length - 1}
+          style={style} 
+          alt={`Link to details for ${name}`}
+          >
+          <h2 className='RestaurantName'>{name}
+            <span className='RestaurantCategory' >{category}</span>
+          </h2>
+        </Link>
+      );
+    });
+  // restaurants = feed_comps
+  // arr = feed_data
   return [restaurants, arr]
 };
 
@@ -36,24 +51,32 @@ export default class LunchTyme extends Component {
     tabOpen: false
   };
 
+  // Off/On Switch to show whether 
+  //  the Details view is open
   toggleTabOpen = () => {
     this.setState({
       tabOpen: !this.state.tabOpen
     })
   };
 
+  // This updates the state after
+  //   when the async fetch completes.
   feedUpdater(restaurants, dataArr) {
     this.setState({
       feed_comps: restaurants,
       feed_data: dataArr,
+      // Setting 'loading' to false renders the feed list.
       loading: false
     });
   };
 
   async componentDidMount() {
+    // response + json fetch and parse the data
     const response = await fetch(dataUrl);
     const json = await response.json();
+    // stateData calls for the creation of the feed_comps
     const stateData = await createFeed(json);
+    // update the state w/ the arr returned from stateData promise
     await this.feedUpdater(stateData[0], stateData[1]);
   };
 
@@ -61,11 +84,11 @@ export default class LunchTyme extends Component {
     const {feed_comps, feed_data, tabOpen, loading} = this.state;
     return (
       <React.Fragment>
-        <AppBar backButton={tabOpen}/>
+        <AppBar backButton={tabOpen} toggle={this.toggleTabOpen}/>
         <div className='RestaurantFeed'>
           {loading
-            ? <Loader active/>
-            : feed_comps }
+            ? <Loader content='Finding tasty lunches...' active/>
+            : feed_comps}
         </div>
         <div className='DetailsWrapper'>
           <Route
